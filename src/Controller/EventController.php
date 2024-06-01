2d77a57ec70288107f2a7ae511e950f9ab4e77ed
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2024-06-01 12:45:00 +0200

    The processing of information about events from IoT devices has been completed, in accordance with the task assumptions

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,35 +3,73 @@
 namespace App\Controller;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class EventController extends AbstractFOSRestController
 {
-  #[Rest\Get('/events', name:"getEvents")]
-  public function getEventsAction() :JsonResponse
-  {
-    return $this->json([
-      'message' => 'Welcome to your get Events controller!',
-      'path' => 'src/Controller/EventController.php',
-    ]);
-  }
-  #[Rest\Get('/events/{id}', name:"getEvent")]
-  public function getEventAction(int $id) :JsonResponse
-  {
-    return $this->json([
-      'message' => 'Welcome to your get Event controller!',
-      'path' => 'src/Controller/EventController.php',
-    ]);
-  }
 
-  #[Rest\Post('/events', name:"postEvent")]
-  public function postEventAction() :JsonResponse
+  #[Rest\Post('/event', name: "postEvent")]
+  public function postEventAction(Request $request)
   {
-    return $this->json([
-      'message' => 'Welcome to your post controller!',
-      'path' => 'src/Controller/EventController.php',
-    ]);
+    $json_data = json_decode($request->getContent(), true);
+    $deviceId = $json_data['deviceId'];
+    $milliseconds = $json_data['eventDate'];
+    $eventDate = date("d/m/Y H:i:s", $milliseconds / 1000);
+    $type = $json_data['type'];
+    $evtData = $json_data['evtData'];
+    if (is_string($deviceId) && $eventDate && is_string(($type))) {
+      $data = [
+        'deviceId' => $deviceId,
+        'eventDate' => $eventDate,
+        'type' => $type
+      ];
+      if ($type == 'deviceMalfunction') {
+        if (is_int($evtData['reasonCode']) && is_string($evtData['reasonText'])) {
+          $data['evtData'] = $evtData;
+          echo 'Event log, sending e-mail and text message';
+        } else {
+          $data['evtData'] = [
+            "reasonCode" => 'Should be an integer',
+            "reasonText" => 'Should be a string'
+          ];
+          return $this->view($data, statusCode: Response::HTTP_BAD_REQUEST);
+        }
+      } elseif ($type == 'temperatureExceeded') {
+        if (is_float($evtData['temp']) && is_float($evtData['treshold'])) {
+          $data['evtData'] = $evtData;
+          echo 'Event log and sending a REST request';
+        } else {
+          $data['evtData'] = [
+            "temp" => 'Should be a float',
+            "treshold" => 'Should be a float'
+          ];
+          return $this->view($data, statusCode: Response::HTTP_BAD_REQUEST);
+        }
+      } elseif ($type == 'doorUnlocked') {
+        if (date("d/m/Y H:i:s", $evtData['unlockDate'] / 1000)) {
+          $data['evtData'] = $evtData;
+          echo 'Event log and text message';
+        } else {
+          $data['evtData'] = [
+            "unlockDate" => 'Should be a timestamp'
+          ];
+          return $this->view($data, statusCode: Response::HTTP_BAD_REQUEST);
+        }
+      } else {
+        return $this->view(['type' => 'Unknown event Type'], statusCode: Response::HTTP_BAD_REQUEST);
+      }
+      return $this->view($data, statusCode: Response::HTTP_OK);
+    }
+
+    return $this->view(
+      [
+        'deviceId' => 'Should be a string',
+        'eventDate' => 'Should be a timestamp',
+        'type' => 'Should be a string'
+      ],
+      statusCode: Response::HTTP_BAD_REQUEST
+    );
   }
 }
